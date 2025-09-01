5af1441854305169fe656a40d28ee5ad7aff37c6
rebel47 <aayyaazz123@gmail.com>
2025-09-01 04:09:40 +0200

    second push

--- lib/services/analytics_service.dart
+++ lib/services/analytics_service.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 
 class AnalyticsService {
   AnalyticsService._privateConstructor();
--- lib/widgets/game_ui/floating_glass_panel.dart
+++ lib/widgets/game_ui/floating_glass_panel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class FloatingGlassPanel extends StatelessWidget {
   final Widget child;
